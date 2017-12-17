2f71442441c0635363f31deba93aef3a0342d3cb
Kevin English <kevin.english@ecoatm.com>
2017-12-16 20:32:35 -0800

    Updates the deploy.sh file

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 git subtree add --prefix=public git@github.com:slqselina/slqselina.github.io.git master --squash
+git subtree pull -q -m"pull latest master `date "+%Y-%m-%d-%H%M%S"`" --prefix=public git@github.com:slqselina/slqselina.github.io.git master
 
+# prepare public folder
+rm -R public/*
+git co public/CNAME
+hugo -t hugo-future-imperfect
 
-
+git status
+# Add file and push
+git add -A
+git commit -a -m "Site Deploy for master `date "+%Y-%m-%d-%H%M%S"`" && git push origin hugo
+git subtree push --prefix=public git@github.com:slqselina/slqselina.github.io.git master
