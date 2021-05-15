2887c6544831c706555a934ff0fd01aacecd641c
Aki Su <slqselina@gmail.com>
2021-05-15 16:05:04 -0700

    Site Deploy for master 2021-05-15-160501

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 chmod -R 755 public/*
 
-git subtree add --prefix=public git@github.com:slqselina/slqselina.github.io.git master --squash
-git subtree pull -q -m"pull latest master `date "+%Y-%m-%d-%H%M%S"`" --prefix=public git@github.com:slqselina/slqselina.github.io.git master
+git subtree add --prefix=public slqselina@github.com:slqselina/slqselina.github.io.git master --squash
+git subtree pull -q -m"pull latest master `date "+%Y-%m-%d-%H%M%S"`" --prefix=public slqselina@github.com:slqselina/slqselina.github.io.git master
 
 # prepare public folder
 rm -R public/*
@@ -14,4 +14,4 @@ git status
 git add -A
 git commit -a -m "Site Deploy for master `date "+%Y-%m-%d-%H%M%S"`"
 git push origin hugo
-git subtree push --prefix=public git@github.com:slqselina/slqselina.github.io.git master
+git subtree push --prefix=public slqselina@github.com:slqselina/slqselina.github.io.git master
